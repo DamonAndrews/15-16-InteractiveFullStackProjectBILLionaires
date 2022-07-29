8e0db6ad4a8e33721678b3013242199d9a4e4cbe
Stephen Heinen <stephenheinen22@yahoo.com>
2022-07-29 15:37:18 -0500

    finished devData seed

--- seeds/devData.js
+++ seeds/devData.js
@@ -7,6 +7,34 @@ const devData = [
         email: 'd.alsakkaf@billionaires.com',
         password: 'test123',
         level: 'Senior',
+    },
+    {
+        first_name: 'Stephen',
+        last_name: 'Heinen',
+        email: 's.heinen@billionaires.com',
+        password: 'test1234',
+        level: 'Mid-Level',
+    },
+    {
+        first_name: 'Damon',
+        last_name: 'Andrews',
+        email: 'd.andrews@billionaires.com',
+        password: 'test12345',
+        level: 'Senior',
+    },
+    {
+        first_name: 'Corey',
+        last_name: 'Stuart',
+        email: 'c.stuart@billionaires.com',
+        password: 'test123456',
+        level: 'Mid-Level',
+    },
+    {
+        first_name: 'Chris',
+        last_name: 'Tolnai',
+        email: 'c.tolnai@billionaires.com',
+        password: 'test1234567',
+        level: 'Junior',
     }
 ]
 
