46d1e5849c498d9731582c827b86e64a497aa596
Stephen Heinen <stephenheinen22@yahoo.com>
2022-07-29 02:51:15 -0500

    tried that in a seperate VS Code window, my ideas did not work, switching back to PDFKit

--- public/js/generatePDF.js
+++ public/js/generatePDF.js
@@ -1,20 +1,4 @@
 // not sure where to put this yet but i know it's all necessary for the pdf convert to work.
-const puppeteer = require("puppeteer");
-const handlebars = require("handlebars");
-
-module.exports.html_to_pdf = async ({ templateHtml, dataBinding, options }) => {
-  const template = handlebars.compile(templateHtml);
-  const finalHtml = encodeURIComponent(template(dataBinding));
-
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox"],
-    headless: true,
-  });
-  const page = await browser.newPage();
-  await page.goto(`data:text/html;charset=UTF-8,${finalHtml}`, {
-    waitUntil: "networkidle0",
-  });
-  await page.pdf(options);
-  await browser.close();
-};
+const PDFDocument = require('pdfkit');
+const doc = new PDFDocument;
 // not sure where to put this yet but i know it's all necessary for the pdf convert to work
