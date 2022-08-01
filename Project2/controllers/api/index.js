c351de1cbd077532bb6ab7a6e1550ccee692f8e6
Stephen Heinen <stephenheinen22@yahoo.com>
2022-08-01 15:39:56 -0500

    commit before pull

--- Project2/controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const developerRoutes = require('./developerRoutes');
-
-
-
-router.use('/', developerRoutes);
-
-module.exports = router;
--- Project2/db/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS contractorportal_db;
-CREATE DATABASE contractorportal_db;
-
