edf78e0e2422d5254ba38c4ac0401b9c74a046d8
Damon Andrews <damonlovesufc@gmail.com>
2022-08-01 17:12:43 -0400

    More clean up

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-DROP DATABASE IF EXISTS contractorportal_db;
-CREATE DATABASE contractorportal_db;
-
-=======
 DROP DATABASE IF EXISTS contractorportal_db;
 CREATE DATABASE contractorportal_db;
 
 USE contractorportal_db
 
->>>>>>> 776600d9976f67d3a8cdcb6f2c872664762c5c3d
